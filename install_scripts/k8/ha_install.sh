fac3167d3e4aaf01916f17708e81e20f9d338753
Katari1 <Vikram.Nagrani@gmail.com>
2018-09-25 10:45:55 -0400

    Added collors

--- install_scripts/k8/ha_install.sh
+++ install_scripts/k8/ha_install.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
+#colors
+WHITE='\033[1;37m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
 
-
-echo "*******************Creating samespace sysdigcloud*******************"
+echo -e  "${GREEN}*******************Creating samespace sysdigcloud*******************${NC}"
 kubectl create namespace sysdigcloud
 
-echo "*******************Creating configmap from sysdigcloud/config.yaml*******************"
+echo -e "${GREEN}*******************Creating configmap from sysdigcloud/config.yaml*******************${NC}"
 kubectl -n sysdigcloud create -f sysdigcloud/config.yaml
 
-echo "*******************Creating quay secret*******************"
+echo -e "${GREEN}*******************Creating quay secret*******************${NC}"
 kubectl -n sysdigcloud create -f sysdigcloud/pull-secret.yaml
 
 #echo "Creating SSL certificates"
 #openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/C=US/ST=CA/L=SanFrancisco/O=ICT/CN=onprem.sysdigcloud.com" -keyout server.key -out server.crt
 
-echo "*******************Creating k8 ssl secret*******************"
+echo -e "${GREEN}*******************Creating k8 ssl secret*******************${NC}"
 kubectl -n sysdigcloud create secret tls sysdigcloud-ssl-secret --cert=server.crt --key=server.key
 
 #echo "Creating storageclass"
 #kubectl apply -f storageclasses/sdc-storageclass.yaml
 
-echo "*******************Creating cassandra pods*******************"
+echo -e "${GREEN}*******************Creating cassandra pods*******************${NC}"
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/cassandra/cassandra-service.yaml
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/cassandra/cassandra-statefulset.yaml
 
-echo "*******************Creating elastic search pods*******************"
+echo -e "${GREEN}*******************Creating elastic search pods*******************${NC}"
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/elasticsearch/elasticsearch-service.yaml
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/elasticsearch/elasticsearch-statefulset.yaml
 
-echo "*******************Creating Mysql Instances*******************"
+echo -e "${GREEN}*******************Creating Mysql Instances*******************${NC}"
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/mysql/mysql-cluster-statefulset.yaml
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/mysql/mysql-router-statefulset.yaml
 
-echo "*******************Creating Redis Instances*******************"
+echo -e "${GREEN}*******************Creating Redis Instances*******************${NC}"
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/redis/redis-primary-statefulset.yaml
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/redis/redis-primary-svc-statefulset.yaml
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/redis/redis-secondary-statefulset.yaml
@@ -40,61 +45,62 @@ kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/redis/r
 kubectl -n sysdigcloud create -f datastores/as_kubernetes_pods/manifests/redis/redis-sentinel-svc-statefulset.yaml
 
 
-echo "*******************Sleeping for two minutes*******************"
+echo -e "${RED}*******************Sleeping for two minutes*******************${NC}"
 sleep 120s
 
-echo "*******************Deploying backend*******************"
+echo -e "${GREEN}*******************Deploying backend*******************${NC}"
 kubectl -n sysdigcloud create -f sysdigcloud/sdc-api.yaml
-echo "*******************Sleeping for one minute*******************"
+echo -e "${RED}*******************Sleeping for one minute*******************${NC}"
 sleep 60s
 kubectl -n sysdigcloud create -f sysdigcloud/sdc-collector.yaml
 kubectl -n sysdigcloud create -f sysdigcloud/sdc-worker.yaml
 
-echo "*******************Creating loadbalancer for API and Collector*******************"
+echo -e "${GREEN}*******************Creating loadbalancer for API and Collector*******************${NC}"
 kubectl -n sysdigcloud create -f sysdigcloud/api-loadbalancer-service.yaml
 kubectl -n sysdigcloud create -f sysdigcloud/collector-loadbalancer-service.yaml
 
-echo "*******************Letting everything catch up (DNS for LB)*******************"
-sleep 120s
+echo -e "${RED}*******************Letting everything catch up (DNS for LB)*******************${NC}"
+sleep 240s
 
 
-echo "*******************Deploying Agents*******************"
+echo -e "${GREEN}}*******************Deploying Agents*******************${NC}"
 api_url=$(kubectl get services -o wide -n sysdigcloud | grep api | awk {'print $4'})
 collector_url=$(kubectl get services -o wide -n sysdigcloud | grep collector | awk {'print $4'})
 cce=$(grep collector: agents/agent_config_ha.yaml)
 
-echo "******************* Your URL is https://$api_url******************************"
+echo -e  "${PURPLE}******************* Your URL is https://$api_url******************************${NC}"
 
 accessKey=$(curl -s -k "https://$api_url:443/api/login" -H 'X-Sysdig-Product: SDC' -H 'Content-Type: application/json' --compressed --data-binary '{"username":"test@sysdig.com","password":"test"}' | jq . | grep accessKey | awk 'NR==1 {print $2}')
 
 accessKey=$(echo $accessKey | sed -e 's/^"//' -e 's/"$//' <<<"$accessKey")
 
-echo "*******************Your Access Key is: $accessKey*******************"
+echo -e "${PURPLE}*******************Your Access Key is: $accessKey*******************${NC}"
 
-echo "*******************Your collector URL is: $collector_url*******************"
+echo -e "${PURPLE}*******************Your collector URL is: $collector_url*******************${NC}"
 
-echo "*******************Fixing collector in yaml*******************"
+echo -e "${GREEN}*******************Fixing collector in yaml*******************${NC}"
 sed -i -e "s/$cce/     collector: $collector_url/g" agents/agent_config_ha.yaml
 
 
-echo "*******************Creating namespace sysdig-agents*******************"
+echo -e "${GREEN}*******************Creating namespace sysdig-agents*******************${NC}"
 kubectl create namespace sysdig-agents
 
-echo "*******************creating secret for accessKey*******************"
+echo -e "${GREEN}*******************creating secret for accessKey*******************${NC}"
 kubectl create secret generic sysdig-agent --from-literal=access-key=$accessKey -n sysdig-agents
 
-echo "*******************creating clusterrole*******************"
+echo -e "${GREEN}*******************creating clusterrole*******************${NC}"
 kubectl apply -f agents/agent_clusterrole.yaml -n sysdig-agents
 
-echo "*******************creating service account*******************"
+echo -e "${GREEN}*******************creating service account*******************${NC}"
 kubectl create serviceaccount sysdig-agent -n sysdig-agents
 
-echo "*******************Creating clusterrole binding*******************"
+echo -e "${GREEN}*******************Creating clusterrole binding*******************${NC}"
 kubectl create clusterrolebinding sysdig-agent --clusterrole=sysdig-agent --serviceaccount=sysdig-agents:sysdig-agent
 
-echo "*******************Deploying Agent Config*******************"
+echo -e "${GREEN}*******************Deploying Agent Config*******************${NC}"
 kubectl apply -f agents/agent_config_ha.yaml -n sysdig-agents
 
-echo "*******************Deploying Agents*******************"
+echo -e "${GREEN}*******************Deploying Agents*******************${NC}"
 kubectl apply -f agents/agent_deployment.yaml -n sysdig-agents
 
+echo -e "${WHITE}*******************It will take about two minutes for the agents to come up.  HAPPY HACKING*******************"
